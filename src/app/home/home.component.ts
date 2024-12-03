ceaaedf425d0e03c98f9367a12a48a0fef522b01
Micro Neko <micronekoch@gmail.com>
2024-12-03 22:09:58 +0800

    Updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  private formBuilder = inject(FormBuilder);
+  public generatedPassword = "";
 
+  generatorForm = this.formBuilder.group({
+    passwordLength: [8],
+    symbols: ["0123456789abcd"],
+  });
+
+  generatePassword() {
+    let generated = "";
+
+    if (!this.generatorForm.value.passwordLength) {
+      return;
+    }
+    if (!this.generatorForm.value.symbols) {
+      return;
+    }
+
+    for (let i = 0; i < this.generatorForm.value.passwordLength; i++) {
+      const position = Math.floor(Math.random() * this.generatorForm.value.symbols.length);
+      generated += this.generatorForm.value.symbols.substring(position, position + 1);
+    }
+
+    this.generatedPassword = generated;
+  }
 }
